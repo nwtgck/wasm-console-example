56d645abbaafa9788a929ed1d220a401dcf4dc66
Ryo Ota <nwtgck@nwtgck.org>
2020-08-09 13:07:04 +0900

    update wasm-console and use new functionalities

--- src/lib.rs
+++ src/lib.rs
@@ -5,4 +5,8 @@ use wasm_console;
 #[wasm_bindgen]
 pub fn use_wasm_console() {
     wasm_console::log!("Hi!", true, 1.3);
+    wasm_console::debug!("Hi!", true, 1.3);
+    wasm_console::info!("Hi!", true, 1.3);
+    wasm_console::warn!("Hi!", true, 1.3);
+    wasm_console::error!("Hi!", true, 1.3);
 }
